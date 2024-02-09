b6578e74d843b4270066693edf6de10d1a0e2aba
blackcatten sweden <m_arnesson@hotmail.com>
2024-02-09 20:48:55 +0000

    add fuction showImage in js

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+function showImage (choice) {
+    document.getElementById('image-rock').style.display = 'none';
+    document.getElementById('image-paper').style.display = 'none';
+    document.getElementById('image-scissor').style.display = 'none';
+}
