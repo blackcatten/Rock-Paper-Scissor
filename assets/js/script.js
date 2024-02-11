a505763c5141536874d0e3874ac695d29dece526
blackcatten sweden <m_arnesson@hotmail.com>
2024-02-11 19:44:56 +0000

    add function show image to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,12 @@ function showImage (choice) {
     document.getElementById('image-rock').style.display = 'none';
     document.getElementById('image-paper').style.display = 'none';
     document.getElementById('image-scissor').style.display = 'none';
+
+if (choice===0) {
+document.getElementById('image-rock').style.display = 'block';
+} else if (choice === 1) {
+    document.getElementById('image-paper').style.display = 'block';
+} else if (choice === 2) {
+    document.getElementById('image-scissor').style.display = 'block';
+}
 }
