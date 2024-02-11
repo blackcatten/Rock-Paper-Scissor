75da637a1be101f46365f03945131429d489274d
blackcatten sweden <m_arnesson@hotmail.com>
2024-02-11 19:58:08 +0000

    add display none to css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
-function showImage (choice) {
+function showImage(choice) {
     document.getElementById('image-rock').style.display = 'none';
     document.getElementById('image-paper').style.display = 'none';
     document.getElementById('image-scissor').style.display = 'none';
 
-if (choice===0) {
-document.getElementById('image-rock').style.display = 'block';
-} else if (choice === 1) {
-    document.getElementById('image-paper').style.display = 'block';
-} else if (choice === 2) {
-    document.getElementById('image-scissor').style.display = 'block';
-}
+    if (choice === 0) {
+        document.getElementById('image-rock').style.display = 'block';
+    } else if (choice === 1) {
+        document.getElementById('image-paper').style.display = 'block';
+    } else if (choice === 2) {
+        document.getElementById('image-scissor').style.display = 'block';
+    }
 }
