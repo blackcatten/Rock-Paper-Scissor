f1c252bd4c8963e1a4253026d4b2348b20f7e420
blackcatten sweden <m_arnesson@hotmail.com>
2024-02-13 11:08:32 +0000

    add function updatescore to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 const choices = ["rock", "paper", "scissors"]
+var playerScore = 0;
+var computerScore = 0;
 
 function showImage(choice) {
     document.getElementById('image-rock').style.display = 'none';
@@ -24,5 +26,37 @@ function showImage(choice) {
         document.getElementById('computer-image-paper').style.display = 'block';
     } else if (computerChoice === 2) {
         document.getElementById('computer-image-scissors').style.display = 'block';
+    }
+    updateScore(choice, computerChoice);
+}
+
+function updateScore(playerChoice, computerChoice) {
+    if (
+        (playerChoice === 0 && computerChoice === 2) ||
+        (playerChoice === 1 && computerChoice === 0) ||
+        (playerChoice === 2 && computerChoice === 1)
+    ) {
+        playerScore++;
+    } else if (
+        (computerChoice === 0 && playerChoice === 2) ||
+        (computerChoice === 1 && playerChoice === 0) ||
+        (computerChoice === 2 && playerChoice === 1)
+    ) {
+        computerScore++;
+    }
+
+    document.getElementById('player-score').innerText = playerScore;
+    document.getElementById('computer-score').innerText = computerScore;
+
+    if (playerScore === 3) {
+        playerWinner(playerScore === 3 ? 'Player' : 'Computer');
+        
+        playerScore = 0;
+        computerScore = 0;
+    } else if (computerScore === 3) {
+        computerWinner(playerScore === 3 ? 'Player' : 'Computer');
+        
+        playerScore = 0;
+        computerScore = 0;
     }
 }
