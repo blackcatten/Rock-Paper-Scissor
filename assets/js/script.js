536bcf82b341efed156d722c71f2a528ff6a9f24
blackcatten sweden <m_arnesson@hotmail.com>
2024-02-13 11:02:35 +0000

    add computers choice to function show image

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,4 +12,17 @@ function showImage(choice) {
     } else if (choice === 2) {
         document.getElementById('image-scissors').style.display = 'block';
     }
+    var computerChoice = Math.floor(Math.random() * 3);
+
+    document.getElementById('computer-image-rock').style.display = 'none';
+    document.getElementById('computer-image-paper').style.display = 'none';
+    document.getElementById('computer-image-scissors').style.display = 'none';
+
+    if (computerChoice === 0) {
+        document.getElementById('computer-image-rock').style.display = 'block';
+    } else if (computerChoice === 1) {
+        document.getElementById('computer-image-paper').style.display = 'block';
+    } else if (computerChoice === 2) {
+        document.getElementById('computer-image-scissors').style.display = 'block';
+    }
 }
